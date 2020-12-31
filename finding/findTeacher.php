6fc830489b4a3493d8bbab66ca97431ebe31c534
Kavya K Kamath <kavya8310023492@gmail.com>
2020-12-31 13:09:56 +0530

    Update findTeacher.php

--- finding/findTeacher.php
+++ finding/findTeacher.php
@@ -39,6 +39,140 @@ if(isset($_POST['search']))
 <html>
     <head>
         <title>PHP HTML TABLE DATA SEARCH</title>
+
+        <meta charset ="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <style>
+
+            .imgCenter{
+                display: block;
+                margin-left: auto;
+                margin-right: auto;
+                width: 800px;
+                height: 400px;
+            }
+
+            #conDetail{
+                margin-top: 50px;                
+                background-color:rgb(232,232,232)  ; 
+            
+
+            }
+
+            #foot{
+                width: 100%;
+                margin-top: 50px;                
+                background-color:rgb(0, 0, 0)  ; 
+                color: #fff;
+                text-align: center;
+                font-weight: var(--font-semi);
+                height: 100px;
+                bottom: 0px;
+                position: absolute;
+
+            
+
+            }
+            #photoDetail{
+                margin-top: 50px;  
+
+            }
+
+            .avatar {
+                vertical-align: middle;
+                width: 35px;
+                height: 35px;
+                border-radius: 50%;
+                margin-top: 8px;
+            }
+
+            .dropdown{
+                position: relative;
+                display: inline-block;
+            }
+            .dropdown-content {
+                display: none;
+                position: absolute;
+                background-color: #f9f9f9;
+                margin-right: 10px;
+                min-width: 180px;
+                /* height: 100px; */
+                
+                box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+                z-index: 1;
+                }
+
+                .dropdown:hover .dropdown-content {
+                display: block;
+                }
+
+                .desc {
+                padding: 15px;
+                text-align: center;
+                }
+
+        </style>
+        <link rel="stylesheet" href="assets/css/navBarStyle.css">
+        <link rel="stylesheet" href="assets/css/footer.css">
+         <!-- =====BOX ICONS===== -->
+        <link href='https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css' rel='stylesheet'>
+
+        
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+        <nav class="navbar navbar-inverse">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavBar">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">
+                        <img style="max-width:50px; margin-top: 1px;" class="img-circle" src="assets/img/logo2.jpg">
+                    </a>
+                </div>
+                <div class="collapse navbar-collapse" id="myNavBar">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="home.html">Home</a></li>
+                        <li><a href="findStudent.php">Student</a></li>
+                        <li><a href="findTeacher.php">Faculty</a></li>
+                        <li ><a href="about.html">About</a></li>
+                    </ul>
+                     <ul class="nav navbar-nav navbar-right">
+                        <div class="dropdown">
+                            <img src="assets/img/avatar.png" alt="Avatar" class="avatar">
+                            
+                            <div class="dropdown-content">
+
+                                <li style="size: 250px;"><a href="studentProfile.php"><span class="glyphicon glyphicon-user"></span> Student Profile</a></li>
+                                <li style="size: 250px;"><a href="teacherProfile.php"><span class="glyphicon glyphicon-user"></span> Teacher Profile</a></li>
+                                
+                                <!-- <li style="size: 250px;"><a href="authentication/registration/register.html"><span class="glyphicon glyphicon-user"></span> Register</a></li> -->
+                                <li style="size: 250px;"><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li> 
+                                
+                                
+                                
+                                <!-- <li style="size: 250px;"><a href="authentication/signIn_SignOut/login.html"><span class="glyphicon glyphicon-log-out"></span> Login</a></li> 
+                                 -->
+                     
+
+                            </div>
+                        
+                        
+                        </div>
+                        <!-- <img src="assets/img/avatar.png" alt="Avatar" class="avatar"> -->
+                        <!-- <li><a href="authentication/registration/register.html"><span class="glyphicon glyphicon-user"></span> Register</a></li>
+                        <li><a href="authentication/signIn_SignOut/login.html"><span class="glyphicon glyphicon-log-out"></span> Login</a></li> --> -->
+                     </ul> 
+                </div>
+            </div>
+        </nav>
+
+
+
         <style>
             table,tr,th,td
             {
@@ -64,7 +198,7 @@ if(isset($_POST['search']))
         <th>address</th>
     </tr>
 <?php
-session_start();
+
 $con=mysqli_connect('127.0.0.1','root','');
 mysqli_select_db($con,'userreg');
 
@@ -102,7 +236,19 @@ while($res=mysqli_fetch_array($query)){
         </form>
         <?php print("$output");
          ?>
+
+
+
+
+<div id ="foot">
+            <p class="footer__title">Online Tutor<br>
+            <div class="footer__social">
+                <a href="#" class="footer__icon"><i class='bx bxl-facebook'></i></a>
+                <a href="#" class="footer__icon"><i class='bx bxl-instagram'></i></a>
+                <a href="#" class="footer__icon"><i class='bx bxl-twitter'></i></a>
+            </div>
+            <p>&#169; 2020 copyright all right reserved</p>
+        </div>
         
          </body>
      </html>
-
